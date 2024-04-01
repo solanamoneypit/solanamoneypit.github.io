6fc39866f898c591b0813999d37b50f8b15d4c30
moneypit <purest_cursors0y@icloud.com>
2024-03-31 22:49:03 -0500

    initial commit of lander

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const joinButton = document.querySelector('#hero button');
+    joinButton.addEventListener('click', function() {
+        alert('Welcome to MoneyPit. Embrace the Void!');
+    });
+});
